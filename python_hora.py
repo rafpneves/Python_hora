7ca38862298ef36e9c7968e65254a6260f419016
ascoid <rafahove2@gmail.com>
2022-08-06 07:54:06 -0300

    06/08/2022

--- python_hora.py
+++ python_hora.py
@@ -1,4 +1,5 @@
 from cgitb import enable
+from distutils.command.clean import clean
 import subprocess
 import requests
 import os
@@ -7,23 +8,54 @@ from datetime import datetime
 from subprocess import PIPE
 
 # ========================================================================================================
-# [global_coleta] pegar o tokem temporario para utilizar na API
-def global_hora():
-  global time_interval
+# [Clear] 
+def Clear():
+  subprocess.run(["clear"])
+
+# ========================================================================================================
+# [Logo] 
+def Logo():
+  print("")
+  print("                                   888    888                      d8888 ")
+  print("                                   888    888                     d88888 ")
+  print("                                   888    888                    d88P888 ")
+  print("                                   8888888888  .d88b.  888d888  d88P 888 ")
+  print("                                   888    888 d88  88b 888P    d88P  888 ")
+  print("                                   888    888 888  888 888    d88P   888 ")
+  print("                                   888    888 Y88..88P 888   d8888888888 ")
+  print("                                   888    888   Y88P   888  d88P     888 ")
+  print("")
+
+# ========================================================================================================
+# [Hora] pegar o tokem temporario para utilizar na API
+def Hora():
+
   path = r"dado.txt"
+  #print(path)
+  
   ti_m = os.path.getatime(path)
+  #print(f"meta dados:               {ti_m}")
+  
   m_ti = time.ctime(ti_m)
+  #print(f"Hora do arquivo {path}: {m_ti}")
+  
   t_obj = time.strptime(m_ti)
+  #print(f"Hora time.struct_time:    {t_obj}")
+  
   T_Doc = time.strftime("%Y-%m-%d %H:%M:%S", t_obj)
+  #print(f"Hora do arquivo {path}: {T_Doc}")
 
   # Filtrando hora
   T_Doc = (T_Doc[11:])
+  #print(f"Hora filtrada:            {T_Doc}")
   
   # Pegando hora atual
   T_hora = str(datetime.today())
+  #print(f"Hora do Servidor:         {T_hora}")
   
   # Filtrando Hora atual
   T_hora = (T_hora[11:19])
+  #print(f"Hora filtrada:            {T_hora}")
 
   # Convertendo as variaveis para [int]
   time_1 = datetime.strptime(T_Doc, "%H:%M:%S")
@@ -37,28 +69,30 @@ def global_hora():
   else:
     if time_interval > "0:00:01": # Teste correto e [ < ]
       print("                         ---------------------------------------------------------")
-      print("                                      === Verificador de Token_temp ===")
+      print("                                        === Verificador de Hora ===")
       print(f"                                         Hora do arquivo: {T_Doc}")
       print(f"                                            Hora atual: {T_hora}")
       print(f"                                    Hora de ultima atualização: {time_interval}\n")
-      print("                                     { Token Erro - Criando novo Token... }\n")
+      print("                               { Tempo do arquivo inrregular Exe arquivo... }\n")
       process = subprocess.Popen("./ping.sh",stdout=PIPE, stderr=PIPE)
       output, error = process.communicate()
-      print("                                          === [ Token Valido ] ===\n")
+      print("                                          === [ Script Rodado ] ===\n")
       subprocess.run(["cat" , "dado.txt"])
       process.kill
     else:
       print("                         ---------------------------------------------------------")
-      print("                                     === Verificador de Token_temp ===")
+      print("                                        === Verificador de Hora ===")
       print(f"                                         Hora do arquivo: {T_Doc}")
       print(f"                                            Hora atual: {T_hora}")
       print(f"                                    Hora de ultima atualização: {time_interval}\n")
-      print("                                          === [ Token Valido ] ===\n")
+      print("                                     === [ Script dentro do tempo ] ===\n")
       subprocess.run(["cat" , "dado.txt"])
 
 
 if __name__ == "__main__":
-  global_hora()
+  Clear()
+  Logo()
+  Hora()
 
   
 
